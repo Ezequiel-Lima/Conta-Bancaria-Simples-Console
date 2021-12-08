1c134549fcd5cef7dfbde2c13595b1cbb484c316
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2021-12-08 09:15:23 -0300

    Add project files.

---
+++ Exercicio 8/Conta.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercicio_8
+{
+    internal class Conta
+    {
+        private int NumeroDaConta { get; set; }
+        public string TitularDaConta { get; set; }
+        public double Deposito { get; set; }
+
+        public Conta(int numeroDaConta, string titularDaConta)
+        {
+            NumeroDaConta = numeroDaConta;
+            TitularDaConta = titularDaConta;
+        }
+
+        public Conta(int numeroDaConta, string titularDaConta, double deposito) : this(numeroDaConta, titularDaConta)
+        {
+            Deposito = deposito;
+        }
+
+        public void AdicionarNoDeposito(double valor)
+        {
+            Deposito = Deposito + valor;
+        }
+
+        public void RemoverNoDeposito(double valor)
+        {
+            Deposito = (Deposito - valor) - 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " +
+                NumeroDaConta +
+                ", Titular: " +
+                TitularDaConta +
+                ", Saldo: $ " +
+                Deposito;
+        }
+    }
+}
---
+++ Exercicio 8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Exercicio_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Entre com o número da conta: ");
+                int numeroDaConta = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o titular da conta: ");
+                string titular = Console.ReadLine();
+
+                char opcao = 'S';
+
+                do
+                {
+                    if(opcao != 'S')
+                    {
+                        Console.WriteLine(">Responda com S ou N");
+                    }
+                    Console.Write("Havera depósito inicial(s/n)? ");
+                    opcao = char.Parse(Console.ReadLine());
+                    opcao = Char.ToUpper(opcao);
+                } while (opcao != 'N' && opcao != 'S');
+
+                Conta conta = new Conta(numeroDaConta, titular);
+                double deposito = 0;
+
+                if (opcao == 'S')
+                {
+                    Console.Write("Entre com o valor do depósito inicial: ");
+                    deposito = double.Parse(Console.ReadLine());
+
+                    conta = new Conta(numeroDaConta, titular, deposito);
+
+                    Console.WriteLine($"\nDados da Conta:\n{conta}");
+                }
+                else
+                {
+                    Console.WriteLine($"\n{conta}");
+                }
+
+                Console.Write("\nEnte um valor para depósito: ");
+                deposito = double.Parse(Console.ReadLine());
+                conta.AdicionarNoDeposito(deposito);
+                Console.WriteLine($"{conta}");
+
+                //Taxa de $5 a cada saque 
+                Console.Write("\nEnte um valor para saque: ");
+                deposito = double.Parse(Console.ReadLine());
+                conta.RemoverNoDeposito(deposito);
+                Console.WriteLine($"{conta}");
+
+                Console.WriteLine("\nPressione qualquer tecla para finalizar");
+                Console.ReadKey();
+            }
+            catch (Exception falha)
+            {
+                Console.WriteLine($">{falha.Message}");
+                Console.ReadKey();
+            } 
+        }
+    }
+}
